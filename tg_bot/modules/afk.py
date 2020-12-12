b744bfe390d509b0d69b9b36662a285303e62fc8
Goqerti <75159910+Goqerti@users.noreply.github.com>
2020-12-12 19:30:10 +0400

    Update afk.py

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -1,158 +1,89 @@
-import random, html
+from typing import Optional
 
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import (DisableAbleCommandHandler,
-                                          DisableAbleMessageHandler)
-from SaitamaRobot.modules.sql import afk_sql as sql
-from SaitamaRobot.modules.users import get_user_id
-from telegram import MessageEntity, Update
-from telegram.error import BadRequest
-from telegram.ext import CallbackContext, Filters, MessageHandler, run_async
+from telegram import Message, Update, Bot, User
+from telegram import MessageEntity
+from telegram.ext import Filters, MessageHandler, run_async
+
+from tg_bot import dispatcher
+from tg_bot.modules.disable import DisableAbleCommandHandler, DisableAbleRegexHandler
+from tg_bot.modules.sql import afk_sql as sql
+from tg_bot.modules.users import get_user_id
 
 AFK_GROUP = 7
 AFK_REPLY_GROUP = 8
 
 
 @run_async
-def afk(update: Update, context: CallbackContext):
+def afk(bot: Bot, update: Update):
     args = update.effective_message.text.split(None, 1)
-    user = update.effective_user
-
-    if not user:  # ignore channels
-        return
-
-    if user.id in [777000, 1087968824]:
-        return
-
-    notice = ""
     if len(args) >= 2:
         reason = args[1]
-        if len(reason) > 100:
-            reason = reason[:100]
-            notice = "\nAFK səbəbi 100 simvoldan çox ola bilməz."
     else:
         reason = ""
 
     sql.set_afk(update.effective_user.id, reason)
-    fname = update.effective_user.first_name
-    try:
-        update.effective_message.reply_text("{} artıq burada deyil!{}".format(
-            fname, notice))
-    except BadRequest:
-        pass
+    update.effective_message.reply_text("{} artıq afk-dır! ".format(update.effective_user.first_name))
 
 
 @run_async
-def no_longer_afk(update: Update, context: CallbackContext):
-    user = update.effective_user
-    message = update.effective_message
+def no_longer_afk(bot: Bot, update: Update):
+    user = update.effective_user  # type: Optional[User]
 
     if not user:  # ignore channels
         return
 
     res = sql.rm_afk(user.id)
     if res:
-        if message.new_chat_members:  #dont say msg
-            return
-        firstname = update.effective_user.first_name
-        try:
-            options = [
-                '{} buradadır!', '{} geri qayıtdı!', '{} artıq qrupdadır!',
-                '{} onlayndır!', '{} artıq onlayndır!', '{} axırki buradadır!',
-                'Xoşgəldin! {}', '{} haradadır?\nHaa qrupdaymış!'
-            ]
-            chosen_option = random.choice(options)
-            update.effective_message.reply_text(chosen_option.format(firstname))
-        except:
-            return
+        update.effective_message.reply_text("{} artıq afk deyi!".format(update.effective_user.first_name))
 
 
 @run_async
-def reply_afk(update: Update, context: CallbackContext):
-    bot = context.bot
-    message = update.effective_message
-    userc = update.effective_user
-    userc_id = userc.id
-    if message.entities and message.parse_entities(
-        [MessageEntity.TEXT_MENTION, MessageEntity.MENTION]):
-        entities = message.parse_entities(
-            [MessageEntity.TEXT_MENTION, MessageEntity.MENTION])
-
-        chk_users = []
+def reply_afk(bot: Bot, update: Update):
+    message = update.effective_message  # type: Optional[Message]
+    if message.entities and message.parse_entities([MessageEntity.TEXT_MENTION, MessageEntity.MENTION]):
+        entities = message.parse_entities([MessageEntity.TEXT_MENTION, MessageEntity.MENTION])
         for ent in entities:
             if ent.type == MessageEntity.TEXT_MENTION:
                 user_id = ent.user.id
                 fst_name = ent.user.first_name
 
-                if user_id in chk_users:
-                    return
-                chk_users.append(user_id)
-
-            if ent.type == MessageEntity.MENTION:
-                user_id = get_user_id(message.text[ent.offset:ent.offset +
-                                                   ent.length])
+            elif ent.type == MessageEntity.MENTION:
+                user_id = get_user_id(message.text[ent.offset:ent.offset + ent.length])
                 if not user_id:
                     # Should never happen, since for a user to become AFK they must have spoken. Maybe changed username?
                     return
-
-                if user_id in chk_users:
-                    return
-                chk_users.append(user_id)
-
-                try:
-                    chat = bot.get_chat(user_id)
-                except BadRequest:
-                    print("Error: Could not fetch userid {} for AFK module"
-                          .format(user_id))
-                    return
+                chat = bot.get_chat(user_id)
                 fst_name = chat.first_name
 
             else:
                 return
 
-            check_afk(update, context, user_id, fst_name, userc_id)
-
-    elif message.reply_to_message:
-        user_id = message.reply_to_message.from_user.id
-        fst_name = message.reply_to_message.from_user.first_name
-        check_afk(update, context, user_id, fst_name, userc_id)
-
-
-def check_afk(update, context, user_id, fst_name, userc_id):
-    if sql.is_afk(user_id):
-        user = sql.check_afk_status(user_id)
-        if not user.reason:
-            if int(userc_id) == int(user_id):
-                return
-            res = "{} AFK-dır".format(fst_name)
-            update.effective_message.reply_text(res)
-        else:
-            if int(userc_id) == int(user_id):
-                return
-            res = "{} hazırda AFK-dır.\nSəbəb: <code>{}</code>".format(
-                html.escape(fst_name), html.escape(user.reason))
-            update.effective_message.reply_text(res, parse_mode="html")
+            if sql.is_afk(user_id):
+                user = sql.check_afk_status(user_id)
+                if not user.reason:
+                    res = "{} afk-dır! səbəb :\n{} ".format(fst_name)
+                else:
+                    res = "{} afk-dır! səbəb :\n{}. ".format(fst_name, user.reason)
+                message.reply_text(res)
 
 
 __help__ = """
- • `/afk <səbəb>`*:* sizi afk olaraq qeyd edər.
- • `brb <səbəb>`*:* yuxarıdakı ilə eyni əmrdir amma əmr deyil.
-Siz afk olanda kimsə sizi tag etsə, reply atsa bot ona cavab verəcəy
+ - /afk <səbəb>: AFK olmaq.
+ - Problemlər Olarsa: @Qagasupport 🤗
+
+
+Siz afk olduğunuz zaman sizi tag edənlər haqqında bildiriş almayacaqsız.
 """
 
+__mod_name__ = "AFK"
+
 AFK_HANDLER = DisableAbleCommandHandler("afk", afk)
-AFK_REGEX_HANDLER = DisableAbleMessageHandler(
-    Filters.regex(r"^(?i)brb(.*)$"), afk, friendly="afk")
+AFK_REGEX_HANDLER = DisableAbleRegexHandler("(?i)brb", afk, friendly="afk")
 NO_AFK_HANDLER = MessageHandler(Filters.all & Filters.group, no_longer_afk)
-AFK_REPLY_HANDLER = MessageHandler(Filters.all & Filters.group, reply_afk)
+AFK_REPLY_HANDLER = MessageHandler(Filters.entity(MessageEntity.MENTION) | Filters.entity(MessageEntity.TEXT_MENTION),
+                                   reply_afk)
 
 dispatcher.add_handler(AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
 dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
-
-__mod_name__ = "AFK"
-__command_list__ = ["afk"]
-__handlers__ = [(AFK_HANDLER, AFK_GROUP), (AFK_REGEX_HANDLER, AFK_GROUP),
-                (NO_AFK_HANDLER, AFK_GROUP),
-                (AFK_REPLY_HANDLER, AFK_REPLY_GROUP)]
