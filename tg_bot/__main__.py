c1fadc7581d03ed74b43832f84fe784f37796a28
Goqerti <75159910+Goqerti@users.noreply.github.com>
2020-12-17 17:56:45 +0400

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -137,7 +137,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
                 parse_mode=ParseMode.MARKDOWN)
     else:
-        update.effective_message.reply_text("Ho-Ho-Ho Həpi Niyu yiə Mən burdayam🎅🤗")
+        update.effective_message.reply_text("Ho-Ho-Ho Həpi Niyu yiə Mən burdayam🎅")
 
 
 # for test purposes
