ec23af84190eb0a6a6a31becf99518cdc991f960
Goqerti <75159910+Goqerti@users.noreply.github.com>
2020-12-25 10:53:53 +0400

    Update lyrics.py

--- tg_bot/modules/lyrics.py
+++ tg_bot/modules/lyrics.py
@@ -15,7 +15,7 @@ def lyrics(bot: Bot, update: Update, args: List[str]):
     message = update.effective_message
     text = message.text[len('/lyrics '):]
     song = " ".join(args).split("- ")
-    reply_text = f'Looks up for lyrics'
+    reply_text = f'Musiqinin Sözlərini Axtarır...'
     
     if len(song) == 2:
         while song[1].startswith(" "):
@@ -30,7 +30,7 @@ def lyrics(bot: Bot, update: Update, args: List[str]):
             lyrics = "\n".join(PyLyrics.getLyrics(
                 song[0], song[1]).split("\n")[:20])
         except ValueError as e:
-            return update.effective_message.reply_text("Song %s not found :(" % song[1], failed=True)
+            return update.effective_message.reply_text("Musiqi %s Tapılmadı :(" % song[1], failed=True)
         else:
             lyricstext = LYRICSINFO % (song[0].replace(
                 " ", "_"), song[1].replace(" ", "_"))
@@ -40,7 +40,8 @@ def lyrics(bot: Bot, update: Update, args: List[str]):
 
 
 __help__ = """
- - /lyrics <keyword> Find your favourite songs' lyrics
+ - /lyrics <musiqi> Favori Musiqinizi Yazaraq Həmin Musiqinin Sözlərini Tapa Bilərsininz!
+ - Qeyd:Azərbaycan Musiqilərə Adətən Tapılmır!
 """
 
 __mod_name__ = "Lyrics"
